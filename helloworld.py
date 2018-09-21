b25d29a31c8254111969619bc6d934e4b34843d7
sureshpuram <puram.suresh@gmail.com>
2018-09-21 13:59:23 -0400

    py file

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+import webbrowser
+print("started")
+url = 'https://sureshpuram.github.io/traviscifinal/'
+webbrowser.open_new(url)
+#webbrowser.get('chrome').open_new(a_website)
+print("completed!!")
